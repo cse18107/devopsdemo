b3e5a8a2e51eed8c914518bf1ba216de596801c9
Soumodeep Maity <87257845+cse18107@users.noreply.github.com>
2023-12-10 07:52:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 app.get("/", (req, res) => {
     res.json({
         status: 200,
-        message: 'Message from server'
+        message: 'Message from server nodeapp414'
     });
 });
 
 app.listen(4030, () => {
-    console.log('Server is working on port 4030 nodeapp444');
+    console.log('Server is working on port 4030 nodeapp414');
 })
